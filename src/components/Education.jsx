074f2a496c5012ebfbc0f0c724db05fcd9bd7e66
Richard Suarez <richardmsc24@gmail.com>
2024-07-07 18:50:33 +0200

    testing colors in iphone

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,7 +8,7 @@ export function Education() {
       <h2 className="text-4xl font-bold text-neutral-400 mt-6 mb-3">
         Educación y Formación
       </h2>
-      <div className="certificates-container grid lg:grid-cols-4  pt-16 pb-16 gap-8">
+      <div className="certificates-container grid lg:grid-cols-4  pt-16 pb-16 lg:gap-8">
         {certificates
           .map((certificate) => (
             <EduCard key={certificate.id} certificate={certificate} />
