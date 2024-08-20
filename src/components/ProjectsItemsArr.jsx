8b48f2bccc8269359c7ff9b9500b5fa7ca586fb7
Richard Suarez <richardmsc24@gmail.com>
2024-08-20 11:23:46 +0200

    adding a new project (ecommerce fakestoreapi)

--- src/components/ProjectsItemsArr.jsx
+++ src/components/ProjectsItemsArr.jsx
@@ -1,8 +1,15 @@
 const projects = [
   {
+    title: "TheShop - Ecommerce",
+    image: "https://runmydev.com/wp-content/uploads/2024/08/theShop.jpg",
+    description:
+      "Esta es un ecommerce de práctica desarrollada con la API de fakestoreapi con el fin de poner en practica los conocimientos del consumo de APIs, manejo de rutas, estados, registro e inicio de sesión gracias a Firebase, estilos con Tailwind CSS, manejo del contexto y demás...",
+    url: "https://theshop-ecommerce-project.netlify.app/",
+  },
+  {
     title: "MusiConnect",
     image:
-      "http://runmydev.com/wp-content/uploads/2024/07/musiconnect-project.jpg",
+      "https://runmydev.com/wp-content/uploads/2024/07/musiconnect-project.jpg",
     description:
       "Aplicación web desarrollada con React como proyecto final del Bootcamp en la escuela de programación Develhope. Utiliza la API de Spotify para acceder a información musical con fines académicos y sin ánimo de lucro. La aplicación permite a los usuarios interactuar socialmente con amigos mientras escuchan su música favorita y se mantienen informados sobre las últimas novedades en el mundo de la música.",
     url: "https://musicconnectdevelhopeesp8team2-toni-martinezs-projects.vercel.app/",
@@ -10,7 +17,7 @@ const projects = [
   {
     title: "Run My Dev - Website",
     image:
-      "http://runmydev.com/wp-content/uploads/2024/07/runmydev-project.jpg",
+      "https://runmydev.com/wp-content/uploads/2024/07/runmydev-project.jpg",
     description:
       "Sitio web desarrollado con WordPress para ofrecer servicios de diseño y desarrollo web como parte de los servicios freelance tanto personales como para otras agencias",
     url: "https://runmydev.com/",
